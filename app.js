74b56a43489a0714e508d7922852e93cd78f6de6
Kamil Ciaston <Kamilciaston6@gmail.com>
2017-07-06 16:04:26 +0200

    add logic to percentage tip calculator

--- app.js
+++ app.js
@@ -1,45 +1,45 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+var budgetCalculator = (function() {
+
+  var allButtons = document.querySelectorAll("input[type=button]");
+  var input = document.querySelector("input[type=text]");
+  var result = document.querySelector("#basicCalculator > input:nth-child(20)");
+  var cancel = document.querySelector("#basicCalculator > input:nth-child(19)");
+
+  [...allButtons].forEach(function(el) {
+    el.addEventListener("click", function() {
+      if (el.value == "=" || el.value == "C") {
+        return;
+      } else {
+        input.value = input.value + el.value;
+        return input.value;
+      }
+    });
+  });
+
+  result.addEventListener("click", function() {
+    input.value = eval(input.value);
+  });
+
+  cancel.addEventListener("click", function() {
+    input.value = ""
+  });
+})();
+
+var percentageTipCalculator = (function(){
+var submitForm = document.getElementById("percentageTipCalculator");
+
+ submitForm.addEventListener("submit", function() {
+  event.preventDefault();
+  var inputValueOne = document.querySelector("#amount").value;
+  var inputValueTwo = document.querySelector("#percentage").value;
+
+  var one =  parseInt(inputValueOne);
+  var two =parseInt(inputValueTwo);
+  var tip = one *(two/100);
+  var total = one + tip;
 
 
-
-var allButtons = document.querySelectorAll("input[type=button]");
-var input = document.querySelector("input[type=text]");
-var result = document.querySelector("#basicCalculator > input:nth-child(20)");
-var cancel = document.querySelector("#basicCalculator > input:nth-child(19)");
-
-
-[...allButtons].forEach(function(el){
- el.addEventListener("click",function(){
-   if(el.value=="=" || el.value=="C"){
-     return;
-   } else {
-  input.value=input.value + el.value;
-  console.log(input.value);
-  console.log(typeof(input.value));
-   return input.value;
- }
+  document.querySelector("#tip").value=tip;
+  document.querySelector("#total").value=total;
 });
-
-});
-
-
-result.addEventListener("click",function(){
- input.value=eval(input.value);
-});
-
-
-cancel.addEventListener("click",function(){
-  input.value=""
-});
-
-
-
-
-
-
-
-
-
-
-
-});
+})();
