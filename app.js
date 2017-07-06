b498649fd8dea455ae4b1ba64a89bae22211958b
Kamil Ciaston <Kamilciaston6@gmail.com>
2017-07-06 18:14:32 +0200

    css styling

--- app.js
+++ app.js
@@ -25,21 +25,20 @@ var budgetCalculator = (function() {
   });
 })();
 
-var percentageTipCalculator = (function(){
-var submitForm = document.getElementById("percentageTipCalculator");
+var percentageTipCalculator = (function() {
+  var submitForm = document.getElementById("percentageTipCalculator");
 
- submitForm.addEventListener("submit", function() {
-  event.preventDefault();
-  var inputValueOne = document.querySelector("#amount").value;
-  var inputValueTwo = document.querySelector("#percentage").value;
+  submitForm.addEventListener("submit", function() {
+    event.preventDefault();
+    var inputValueOne = document.querySelector("#amount").value;
+    var inputValueTwo = document.querySelector("#percentage").value;
 
-  var one =  parseInt(inputValueOne);
-  var two =parseInt(inputValueTwo);
-  var tip = one *(two/100);
-  var total = one + tip;
+    var one = parseInt(inputValueOne);
+    var two = parseInt(inputValueTwo);
+    var tip = one * (two / 100);
+    var total = one + tip;
 
-
-  document.querySelector("#tip").value=tip;
-  document.querySelector("#total").value=total;
-});
+    document.querySelector("#tip").value = tip;
+    document.querySelector("#total").value = total;
+  });
 })();
